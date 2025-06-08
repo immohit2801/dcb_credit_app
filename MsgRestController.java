013b4ad9fa0bd10f4d420d152347ff3feb61d97d
Mohit Kumar <immohit2805@gmail.com>
2025-06-08 13:33:34 +0530

    Controller added

---
+++ MsgRestController.java
@@ -0,0 +1,27 @@
+package in.ashokit.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import in.ashokit.service.MsgService;
+
+@RestController
+@RequestMapping("/api")
+public class MsgRestController {
+	
+	@Autowired
+	private MsgService msgService;
+	
+	@GetMapping("/welcome")
+	public String getMsg() {
+		int age= 26;
+		int i = 10;
+		int j = 20;
+		int age = 16;
+		int k = 30;
+		return msgService.getWelcomeMsg();
+	}
+	
+}
